f667bdfb57986e0c1ad999a3a5fcda7837c7ab0c
Aníbal Santos Gómez <anibalsantosgo@gmail.com>
2024-06-01 01:36:52 +0200

    Update install-arch-post-install.sh

--- install-arch-post-install.sh
+++ install-arch-post-install.sh
@@ -6,24 +6,25 @@ sudo pacman -Sy
 # Instalar git
 sudo pacman -S --noconfirm git
 
+echo ""
 echo "Instalando Gnome..."
-
+echo ""
+echo "Gnome Minimal"
 # Instalar Gnome minimal
 sudo pacman -S --noconfirm gdm gnome-control-center gnome-session gnome-settings-daemon gnome-shell gnome-keyring nautilus
+echo ""
+echo "Gnome Utils"
 sudo pacman -S --noconfirm gnome-characters gnome-color-manager gnome-disk-utility gnome-menus gnome-shell-extensions grilo-plugins gvfs gvfs-afc gvfs dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb loupe sushi tracker3-miners xdg-desktop-portal-gnome xdg-user-dirs-gtk
+echo ""
+echo "Gnome Extensions"
+sudo pacman -S --noconfirm gnome-tweaks gnome-shell-extension-dash-to-dock
+
+echo ""
 echo "Instalando paquetes..."
 
 echo "Alacritty"
 
 sudo pacman -S --noconfirm alacritty
-
-echo "Gnome Tweaks"
-
-sudo pacman -S --noconfirm gnome-tweaks
-
-echo "Gnome Dash to Dock"
-
-yay -S --noconfirm gnome-shell-extension-dash-to-dock
 
 echo "Habilitando servicios..."
 
