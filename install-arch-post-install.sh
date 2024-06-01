5e2c876d3bf8d6fbe8d5fe490d2e95176018e1c1
Aníbal Santos Gómez <anibalsantosgo@gmail.com>
2024-06-01 01:34:33 +0000

    Update install-arch-post-install.sh

--- install-arch-post-install.sh
+++ install-arch-post-install.sh
@@ -35,13 +35,11 @@ echo "Instalando Gnome..."
 echo ""
 echo "Gnome Minimal"
 # Instalar Gnome minimal
-sudo pacman -S --noconfirm gdm gnome-control-center gnome-session gnome-settings-daemon gnome-shell gnome-keyring nautilus
-echo ""
-echo "Gnome Utils"
-sudo pacman -S --noconfirm gnome-characters gnome-color-manager gnome-disk-utility gnome-menus gnome-shell-extensions grilo-plugins gvfs gvfs-afc gvfs dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb loupe sushi tracker3-miners xdg-desktop-portal-gnome xdg-user-dirs-gtk
+sudo pacman -S --noconfirm gdm gnome-control-center gnome-session gnome-settings-daemon gnome-shell gnome-keyring nautilus gnome-characters gnome-color-manager gnome-disk-utility gnome-menus gnome-shell-extensions grilo-plugins gvfs gvfs-afc gvfs-dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb loupe sushi tracker3-miners xdg-desktop-portal-gnome xdg-user-dirs-gtk
 echo ""
 echo "Gnome Extensions"
-sudo pacman -S --noconfirm gnome-tweaks gnome-shell-extension-dash-to-dock
+sudo pacman -S --noconfirm gnome-tweaks
+yay - S gnome-shell-extension-dash-to-dock
 
 echo ""
 echo "Instalando paquetes..."
