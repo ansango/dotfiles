cf0c5d153cef2ef23d9228e1ba7d131c9f3e920f
ansango <anibalsantosgo@gmail.com>
2024-10-16 14:58:08 +0000

    up

--- post-install.sh
+++ post-install.sh
@@ -158,7 +158,7 @@ fi
 
 # Copiar archivos de .dotfiles
 log "Copiando archivos de .dotfiles..."
-cp -r ~/dotfiles/. ~/
+cp -r ~/dotfiles/dotfiles. ~/
 
 # Copiar fondos de pantalla
 log "Copiando fondos de pantalla..."
