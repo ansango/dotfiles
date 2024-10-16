76597ecda1a2b38e5645cf5f5a6169fcc3d56bdf
Aníbal Santos Gómez <anibalsantosgo@gmail.com>
2024-10-16 15:42:07 +0200

    Delete install-arch-nvidia.sh

--- install-arch-nvidia.sh
+++
@@ -1,8 +0,0 @@
-
-echo “install i915 /bin/false” | sudo tee --append /etc/modprobe.d/blacklist.conf
-cat /etc/modprobe.d/blacklist.conf
-sudo pacman -Syyuu
-sudo pacman -S xorg-server xorg-xinit xorg-apps --noconfirm
-sudo pacman -S nvidia nvidia-utils nvidia-settings --noconfirm
-cat /usr/lib/modprobe.d/nvidia-utils.conf
-
